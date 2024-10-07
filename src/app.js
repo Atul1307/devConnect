1b7df153019dc17df8e35d9e0ca1be727b728d45
Atul1307 <katul2015@gmail.com>
2024-10-07 20:38:03 +0530

    Updates origin URL for CORS

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://devconnectsocial.netlify.app/',
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
